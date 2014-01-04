e0c17a7e5049c018215b33c0bfe3a11d904189c1
Geoff Shapiro <gshapiro49@gmail.com>
2014-01-04 06:14:28 -0500

    How did I miss this, too?

---
+++ C++/Semester 3/Pythag/SHAPgA2v2.cc
@@ -0,0 +1,55 @@
+// pythVER1.cpp
+// works with nested loops, integer arithmetic and arrays 
+// generates the upper triangular table beautifully, with columns aligned 
+// overflows the screen when m > 798 
+#include <iostream>
+using namespace std;
+#include <conio.h> 
+#include <math.h>
+#include <iomanip> 
+
+int main()
+   {
+   int m, i, j, c;
+   int high;
+   int low;
+   int mm;
+   double xm;
+   int pCan[1000] = {0};
+   int testCounter = 0;
+   
+   cout << "This program generates Pythagorean candidate numbers in a table.\n";
+   cout << "Type integer candidate number size limit m\n";
+   cin >> m;
+   cout << "Type the higher bound limit\n";
+   cin >> high;
+   cout << "Type the lower bound limit\n";
+   cin >> low;
+   cout << endl;
+
+   
+   xm = m + 1;				            //find corresponding table-size mm for this limit
+   mm = sqrt(xm/2.) +2;
+   
+   for ( i=1; i < mm - 1; i++ )         // begin row loop; note row mm [i.e. last] is empty
+   {                                    // handle columns
+	 
+	  for ( j= i + 1; j < mm; j++)      // show p-candidates on rest of row
+	  {  
+		  c = i*i + j*j;
+		  if(c >= low && c <= high) // Only putting the the numbers within the limits in the array.
+			 pCan[testCounter] = c;
+
+		  testCounter++;	
+	  }
+      
+   }  
+   for (int counter = 0; counter < 1001; counter++)
+   {
+	   if(pCan[counter] != 0 && pCan[counter] >= 1) // Logic to not output the excess numbers
+	      cout << pCan[counter] << endl;
+	   
+   }
+   system( "pause");
+   return 0;
+   }  //end main
--- C++/Semester 3/Pythag/SHAPgA2v3.cc
+++ C++/Semester 3/Pythag/SHAPgA2v3.cc
@@ -8,7 +8,6 @@ Deliverable: Assignment #2--- Pythagorean perfect squares
 Description: This program generates a pythaagorean list of numbers, orders them, removes duplicates 
 and then checks which numbers are pythagorean squares 
 
-A Pythagorean number is a number of the form 4n + 1. These are exactly the numbers that can be the hypotenuse of a Pythagorean triangle..
 
 ***********************************************************************************/
 /************************* PRELIMINARIES **************************/
@@ -34,7 +33,7 @@ int main()
    int mm; 
    double xm;
    int pCan[1000] = {0}; // The array to use for storing the C values 
-   int testCounter = 0; // Subscript for pCan[]
+   int testCounter = 0; // The counter I use to know how to fill the pcan array
    
    cout << "This program generates Pythagorean candidate numbers in a table.\n";
 
@@ -49,44 +48,21 @@ int main()
    mm = sqrt(high +1);
    
    for ( i=1; i < high; i++ )         // begin row loop; note row mm [i.e. last] is empty
-   {   for ( j= i + 1; j < mm; j++)      // show p-candidates on rest of row
+   {                                    // handle columns
+	 
+	  for ( j= i + 1; j < mm; j++)      // show p-candidates on rest of row
 	  {   c = i*i + j*j;
 		  if(c >= low && c <= high) // Only putting the the numbers within the limits in the array.
-			{   r = c; 
+			{   
+				r = c; 
 				MakeList(tail, r, pCan);
-				testCounter++;     // Incrementing of subscript
+				testCounter++;
+
 		    }
       }  
 
    }
-
-
-   // Variables I'm going to use just for the perfect square stuff
-   double sq;
-   int perfectSQ;
-   int pythC = 0;
-   int pyth[1000] = {0}; // The array we're going to use to store the pythagorean squares
-
-   // End of vatiables I'm going to be using for the perfect squares
-   cout << "q" << "     " << "p[q]\n \n";
-   for (int count = 0; count < testCounter; count++)
-   {  
-	   sq = sqrt((double)pCan[count]); // Getting the square root of the pCan value
-	   perfectSQ = (int)sq;
-	   if (sq == perfectSQ)
-	   {  
-		   //cout << pCan[count] << " Is a perfect square! \n\n";
-		   pyth[pythC] = pCan[count];
-		   pythC++;
-	   }
-   }
-
-  for (int i = 0; i < testCounter; i++)
-   {   if (pyth[i] != 0)
-          cout << i << setw(7) << pyth[i] << " Is a perfect square\n";
-   }
    
-  
    system( "pause");
    return 0;
    }  //end main
@@ -99,32 +75,49 @@ void MakeList(int &t, int r, int p[])    // I hope you can sense the frustration
    
 
 		if (t == -1)
-		{   p[0] = r; // Putting the the R value into the first section of my array
+		{ 
+			p[0] = r; // Putting the the R value into the first section of my array
 			t = t + 1;
 			f = true;
 		}
 		while (q < t && !f)
-		{   q = q + 1;
+		{
+			q = q + 1;
+
 			if (r <= p[q])
-			{	f = true;
-				if (r != p[q]) // Making sure the value we want to put into the array is unique
-				{   x = t;
-					while (x >= q) // Moves and checkes numbers in the array to find the correct place
-					{   p[x+1] = p[x]; // The move
-						x = x - 1;	// Making X one less so it can keep on moving the value if need be
+			{
+				f = true;
+				if (r != p[q]) // Making sure the value we want to put into the array isn't already in the current place
+				{
+				
+					x = t;
+					while (x >= q) // This is the loop which "moves" and checkes numbers in the array to find the correct place
+					{
+						
+						p[x+1] = p[x]; // The move
+						x = x - 1;			// Making X one less so it can keep on moving the value if need be			
 					}
-                        t = t + 1; // Getting ready for the next number by making tail incriment
+                        t = t + 1; // Getting ready for the next number by making tail jump to the closest empty part
 						p[q] = r;   // We finally put the new value into pCan
 				}
+				
 
 			}
 						
 		}
 	
 	
-	if (f == false) // R is the biggest number we want to insert
-		{   t = t + 1;
+	if (f == false)
+		{
+			t = t + 1;
 			p[t] = r;
+			
 		}
+	for (int count = 0; count < t; count++)
+	{
+		cout << p[count] << endl; // We use this to output the numbers from the list we just made
+		
+	}
+	system("pause");
 	
 }
